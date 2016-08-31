ec3ee90e91ddc3d64b374b7b2e697c4772a5db65
Jason Ish <ish@unx.ca>
2016-08-30 23:35:19 -0600

    move config tests

--- config/config_test.go
+++ config/config_test.go
@@ -1,9 +1,20 @@
-package main
+package config
 
 import (
 	"encoding/json"
 	"testing"
 )
+
+func AssertEquals(t *testing.T, value interface{}, expected interface{}) {
+	if value != expected {
+		if _, ok := value.(int64); ok {
+			t.Fatalf("got %d, expected %d", value, expected)
+		} else {
+			// Fall back to strings.
+			t.Fatalf("got %s, expected %s", value, expected)
+		}
+	}
+}
 
 func TestConfigGetSpoolByName(t *testing.T) {
 	jsonConfig := `{
@@ -17,21 +28,22 @@ func TestConfigGetSpoolByName(t *testing.T) {
   ]
 }`
 
-	config := Config{}
-	err := json.Unmarshal(([]byte)(jsonConfig), &config)
+	_config := Config{}
+	err := json.Unmarshal(([]byte)(jsonConfig), &_config)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	spool := config.GetSpoolByName("first")
+	spool := _config.GetSpoolByName("first")
 	if spool == nil {
 		t.Fatal("spool first not found")
 	}
+
 	AssertEquals(t, spool.Name, "first")
 	AssertEquals(t, spool.Directory, "/capture/first")
 	AssertEquals(t, spool.Prefix, "first.pcap")
 
-	spool = config.GetSpoolByName("second")
+	spool = _config.GetSpoolByName("second")
 	if spool == nil {
 		t.Fatal("spool second not found")
 	}
@@ -39,7 +51,7 @@ func TestConfigGetSpoolByName(t *testing.T) {
 	AssertEquals(t, spool.Directory, "/capture/second")
 	AssertEquals(t, spool.Prefix, "second.pcap")
 
-	spool = config.GetSpoolByName("third")
+	spool = _config.GetSpoolByName("third")
 	if err != nil {
 		t.Fatalf("expected nil instead of a spool")
 	}
